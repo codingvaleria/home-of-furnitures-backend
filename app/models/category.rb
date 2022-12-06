77b4ce4cebfd0be9f4ae8de3b8b87ef58c5efa28
codingvaleria <codingvaleria@gmail.com>
2022-12-06 10:05:10 +0300

    delete category resource

--- app/models/category.rb
+++
@@ -1,4 +0,0 @@
-class Category < ApplicationRecord
-    validates :name, inclusion: { in: %w(Living-Room Dining Kitchen Bedroom), message: "%{value} is not a valid size" }
-    validates :description, length: { minimum: 50 }
-end
