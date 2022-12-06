e672a9489f731ae3dc8c164af85334a4bb41b6bf
codingvaleria <codingvaleria@gmail.com>
2022-12-06 10:22:20 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  
+  resources :products
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
-  root "products#index"
-  resources :products, only: [:index, :show, :create, :update, :destroy]
-  resources :categories, only: [:index, :show, :create, :update, :destroy]
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
